4e313c06b77a684522695f64f50d28a9ae9d1554
Student <student@lcoast.itstep>
2023-12-11 21:28:49 +0600

    django exam project added

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from myapp.views import *
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", blog_post_list, name="blog_post_list"),
+    path("blog/create", blog_post_create, name="blog_post_create"),
+    path("blog/<int:pk>", blog_post_detail, name="blog_post_detail"),
+    path("blog/<int:pk>/update", blog_post_update, name="blog_post_update"),
+    path("blog/<int:pk>/delete", blog_post_delete, name="blog_post_delete"),
+]
